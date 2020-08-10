cd9d370c9442bdb5c621f7afd83c8687f2c1a85f
pedrocaximiliano <pedro_caximiliano@hotmail.com>
2020-08-10 15:52:36 -0300

    create components

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,12 +1,58 @@
 import React from 'react'
 
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Inputs';
+
+import Select from '../../components/Select';
 
 import './styles.css';
 
+import warningIcon from '../../assets/icons/warning.svg'
+import TextArea from '../../components/TextArea';
+
+
 export default function TeacherForm() {
     return (
-        <PageHeader title="Que incrível você quer dar aulas">
-        </PageHeader>
+        <div id="page-teacher-form" className="container">
+            <PageHeader
+                description="O primeiro passo é preencher esse formulário de inscrição"
+                title="Que incrível você quer dar aulas">
+            </PageHeader>
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                        <Input name="name" label="Nome Completo" />
+                        <Input name="avatar" label="Dia da Semana" />
+                        <Input type="number" name="whatsApp" label="whatsApp" />
+                        <TextArea label="Biografia" name="bio"/>
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+                        <Select
+                         name="subject" 
+                         label="Matéria"
+                         options={[
+                            { value: 'Cálculo', label: 'Cálculo' },
+                            { value: 'Lógica de programação', label: 'Lógica de programação' },
+                            { value: 'Algorítimo', label: 'Algorítimo' },
+                            { value: 'Redes', label: 'Redes' },
+                         ]}
+                         />
+                        <Input name="cost" label="custo por hora" />
+                </fieldset>
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso Importe" />
+                            Importante! <br />
+                            Preencha todos os dados
+                    </p>
+                    <button type="button">
+                      Salvar cadastro
+                    </button>
+                </footer>
+            </main>
+        </div>
     )
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,39 +2,42 @@ import React from 'react'
 
 import './styles.css'
 
-import WhatsAppIcon from '../../assets/icons/whatsapp.svg';
-
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Inputs';
+import Select from '../../components/Select';
 
 export default function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os Proffys disponíveis">
                 <form action="" id="search-teachers">
-
-                    <div className="input-block">
-                        <label htmlFor="subject">Matérias</label>
-                        <input type="text" id='subject' />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="week-day">Dias</label>
-                        <input type="text" id='week-day' />
-                    </div>
-
-                    <div className="input-block">
-                        <label htmlFor="time">hora</label>
-                        <input type="text" id='time' />
-                    </div>
-
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: 'Cálculo', label: 'Cálculo' },
+                            { value: 'Lógica de programação', label: 'Lógica de programação' },
+                            { value: 'Algorítimo', label: 'Algorítimo' },
+                            { value: 'Redes', label: 'Redes' },
+                        ]}
+                    />
+                    <Select
+                        name="week_day"
+                        label="Dia da Semana"
+                        options={[
+                            { value: '0', label: 'Segunda-feira' },
+                            { value: '1', label: 'Terça-feira' },
+                            { value: '2', label: 'Quarta-feira' },
+                            { value: '3', label: 'Quinta-feira' },
+                            { value: '4', label: 'Sexta-feira' },
+                            { value: '5', label: 'Sábado' },
+                        ]}
+                    />
+                    <Input type="time" name="time" label="Hora" />
                 </form>
             </PageHeader>
             <main>
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
                 <TeacherItem />
             </main>
         </div>
